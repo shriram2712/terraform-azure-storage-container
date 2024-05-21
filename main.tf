b6a755c0f8534ad59dc9b81ca7b6ee37764793cd
Shriram Rajaraman <32219070+shriram2712@users.noreply.github.com>
2024-05-21 17:43:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "azurerm_storage_account" "tfe_storage_account" {
     24
   )
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azure_resource_group.example.name
 
   account_tier             = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
